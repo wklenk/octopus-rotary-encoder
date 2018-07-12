9f7fdd99b205888d18515e9004f07baf4cebc81a
Wolfgang Klenk <wolfgang.klenk@gmail.com>
2018-07-12 22:03:54 +0200

    First commit.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # octopus-rotary-encoder
-Rotary encoder example for IoT OCTOPUS, the badge for IoT Evaluation by fab-lab.eu
+Rotary encoder example for IoT Octopus, the badge for IoT Evaluation by fab-lab.eu
+
+## External libraries
+This example requires an external Arduino library:
+
+Encoder Library (Version 1.4.1) by PaulStoffregen
+https://www.pjrc.com/teensy/td_libs_Encoder.html
+https://github.com/PaulStoffregen/Encoder
+
+## Hardware purchase
+* Octopus IoT Badge: https://www.tindie.com/products/FabLab/iot-octopus-badge-for-iot-evaluation/
+* Rotary Encoder: https://www.exp-tech.de/sensoren/sonstige/6032/rotary-encoder-extras (Germany) or https://www.adafruit.com/product/377
+
+## Functionality
+* Turning the button to the right or left increases or decreases a counter.
+* Pressing the button resets the counter
 
---
+++ octopus-rotary-encoder.ino
@@ -0,0 +1,64 @@
+/*
+ * rfcc1101 - SPI Protocol Driver for TI CC1101 RF communication module.
+ *
+ * Copyright (C) 2013 Wolfgang Klenk <wolfgang.klenk@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Encoder Library Version 1.4.1
+// https://www.pjrc.com/teensy/td_libs_Encoder.html
+// https://github.com/PaulStoffregen/Encoder
+
+#include <Encoder.h> 
+
+#define ENCODER_PIN_A 14
+#define ENCODER_PIN_B 12
+#define ENCODER_PIN_BUTTON 2
+
+Encoder encoder(ENCODER_PIN_A, ENCODER_PIN_B);
+
+long oldPosition  = -1;
+
+// The setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    // Wait for serial port to connect. Needed for native USB
+    delay(100);
+  }
+  Serial.println(F("Basic Encoder Test:"));  
+  
+  // Initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Push button functionality
+  pinMode(ENCODER_PIN_BUTTON, INPUT);
+}
+
+// The loop function runs over and over again forever
+void loop() {
+
+  if(digitalRead(ENCODER_PIN_BUTTON) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+    encoder.write(0); // Reset to zero
+  }
+  
+  long newPosition = encoder.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+}
